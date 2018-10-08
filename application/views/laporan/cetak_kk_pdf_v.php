fac12adf8d7f112bdf202754b98fb27a575a4936
Yaqub <yaqub.djava@gmail.com>
2018-10-08 09:24:44 +0700

    Laporan KK
    Done

---
+++ application/views/laporan/cetak_kk_pdf_v.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<?php
+error_reporting(0);
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link href="<?php echo base_url('metronic/global/plugins/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
+        <title>Report</title>
+    </head>
+<!-- <style>
+@media print {
+    .popup {page-break-after: always;}
+}
+</style> -->
+
+    <body>
+    <center><h2>Report Data Keluarga</h2></center>
+
+    <div class="popup">
+    <br>
+                <table width="100%" border=0>
+                    
+                    <tr>
+                        <td>
+                        <label>No KK</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->id_master_kk; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="20%">
+                        <label>NIK</label>
+                        </td>
+                        <td width="50%">
+                        <p><?php echo $data_kk->id_ktp; ?></p>
+                        </td>
+                        <!-- <td rowspan="17" valign="top" style="text-align:center">
+                            <img src="<?= site_url().$data_kk->link_gambar; ?>" id="gambar_foto_ktp" alt="" style="border: 1px #000  solid; padding: 15px;"/>    
+                        </td> -->
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Nama</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_ktp; ?></p>
+                        </td>
+                    </tr>
+                    <!-- <tr>
+                        <td>
+                        <label>Tempat lahir</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->tempat_lahir; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Tanggal lahir</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->tanggal_lahir; ?></p>
+                        </td>
+                    </tr> -->
+                    <tr>
+                        <td>
+                        <label>Alamat</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->alamat; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Rt</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->rt; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Rw</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->rw; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Kelurahan</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_kel; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Kecamatan</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_kec; ?></p>
+                        </td>
+                    </tr>
+                    <!-- <tr>
+                        <td>
+                        <label>Jenis kelamin</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_jekel; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Gol darah</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->gol_darah; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Agama</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_agama; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Status kawin</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_nikah; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Pekerjaan</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_pekerjaan; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Difabel</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_difabel; ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <label>Pendidikan</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_pend; ?></p>
+                        </td>
+                    </tr> -->
+                    <tr>
+                        <td>
+                        <label>Hubungan keluarga</label>
+                        </td>
+                        <td>
+                        <p><?php echo $data_kk->nama_hub_kel; ?></p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan=3>&nbsp;</td>
+                    </tr>
+                    <tr style="text-align:center">
+                        <td colspan=3><h3><b>Komunitas</b></h3></td>
+                    </tr>
+                    <tr>
+                        <td colspan=3 class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            Jenis Komunitas
+                                        </th>
+                                        <th>
+                                            Nama Komunitas
+                                        </th>
+                                        <th>
+                                            NIK
+                                        </th>
+                                        <th>
+                                            Nama
+                                        </th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                        $query = $this->db->query("SELECT * from trans_kk where id_master_kk='".$data_kk->id_master_kk."'");
+                                        $ktp= $query->result();
+                                        foreach ($ktp as $row1) {
+                                            $query2 = $this->db->query("SELECT * from vw_komunitas where id_ktp='".$row1->id_ktp."'");
+                                            $komunitas= $query2->result();
+                                            foreach ($komunitas as $row) {
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $row->nama_jeniskomunitas; ?></td>
+                                            <td><?php echo $row->nama_komunitas; ?></td>
+                                            <td><?php echo $row->id_ktp; ?></td>
+                                            <td><?php echo $row->nama_ktp; ?></td>
+                                        </tr>
+                                    <?php
+                                        }
+                                    }
+                                    ?>
+
+                                </tbody>
+                                <tfoot>
+
+                                </tfoot>
+                            </table>                        
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan=3>&nbsp;</td>
+                    </tr>
+                    <tr style="text-align:center">
+                        <td colspan=3><h3><b>Bantuan</b></h3></td>
+                    </tr>
+                    <tr>
+                        <td colspan=3 class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            Tanggal
+                                        </th>
+                                        <th>
+                                            Nama instansi
+                                        </th>
+                                        <th>
+                                            Nama Bantuan
+                                        </th>
+                                        <th>
+                                            NIK
+                                        </th>
+                                        <th>
+                                            Nama
+                                        </th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                        $query = $this->db->query("SELECT * from trans_kk where id_master_kk='".$data_kk->id_master_kk."'");
+                                        $ktp= $query->result();
+                                        foreach ($ktp as $row1) {
+                                            $query = $this->db->query("SELECT * from vw_t_bantuan where id_ktp='".$row1->id_ktp."'");
+                                            $bantuan= $query->result();
+                                            foreach ($bantuan as $row) {
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $row->tgl_bantuan; ?></td>
+                                            <td><?php echo $row->nama_instansi; ?></td>
+                                            <td><?php echo $row->nama_bantuan; ?></td>
+                                            <td><?php echo $row->id_ktp; ?></td>
+                                            <td><?php echo $row->nama_ktp; ?></td>
+                                        </tr>
+                                    <?php
+                                        }
+                                    }
+                                    ?>
+
+                                </tbody>
+                                <tfoot>
+
+                                </tfoot>
+                            </table>                        
+                        </td>
+                    </tr>
+
+
+                </table>
+
+    </div>
+
+    </body>
+</html>
+<!-- <script src="<?php echo base_url('metronic/global/plugins/bootstrap/js/bootstrap.min.js'); ?>" type="text/javascript"></script> -->
+<script>
+    window.print();
+</script>
---
+++ application/views/laporan/lap_kk_v.php
@@ -0,0 +1,122 @@
+
+<!-- BEGIN PAGE BREADCRUMB -->
+<!-- END PAGE BREADCRUMB -->
+<!-- END PAGE HEADER-->
+<!-- BEGIN PAGE CONTENT-->
+<!-- KONTEN DI SINI YA -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="portlet portlet-sortable light bordered">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="fa fa-pencil-square-o font-blue-chambray"></i>
+                    <span class="caption-subject font-blue-chambray bold uppercase"><?php echo $menu_header; ?></span>
+                </div>
+                <div class="actions">
+                    <!-- <a href="javascript:;" class="btn btn-default btn-sm" onclick="download_excel();">
+                        <i class="fa fa-file-excel-o"></i> Download Excel </a>
+                    <a href="javascript:;" class="btn btn-default btn-sm" onclick="cetakall();">
+                        <i class="fa fa-print"></i> Cetak All </a> -->
+                    <a class="btn btn-icon-only btn-default btn-sm fullscreen" href="javascript:;"
+                       data-original-title="" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <div>
+                    <span id="event_result">
+
+                    </span>
+                </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-striped table-bordered table-hover text_kanan" id="idGridSelectPenerima">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            Action
+                                        </th>
+                                        <th>
+                                            No KK
+                                        </th>
+                                        <th>
+                                            NIK
+                                        </th>
+                                        <th>
+                                            Nama
+                                        </th>
+                                        <th>
+                                            Kecamatan
+                                        </th>
+                                        <th>
+                                            Kelurahan
+                                        </th>
+                                        <th>
+                                            Rw
+                                        </th>
+                                        <th>
+                                            Rt
+                                        </th>
+                                        <th>
+                                            Jml Keluarga
+                                        </th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                                <tfoot>
+
+                                </tfoot>
+                            </table>
+
+                        </div>
+
+
+                        <!-- <div class="form-body">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Nama</label>
+                                    <input type="hidden" id="id_ktp">                                    
+                                    <input type="text" id="id_nama" class="form-control input-sm" onclick="onModal()">                                    
+                                </div>   
+                            </div>
+                            <div class="col-md-4">
+
+                            </div>
+                        </div> -->
+                        <!-- HIDDEN INPUT -->
+                        <input type="text" id="idTmpAksiBtn" class="hidden">
+                        <!-- END HIDDEN INPUT -->
+                    </div>
+                    <!--END ROW 1 -->
+
+                    <!-- <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-actions ">
+                                <a href="javascript:;" class="btn blue" onclick="cetak();">
+                                    <i class="fa fa-print"></i> Cetak </a>
+
+                                <button id="id_btnBatal" type="button" class="btn default">Batal</button>
+
+                            </div>
+                        </div>
+                    </div> -->
+
+            </div>
+        </div>
+        <!-- end <div class="portlet green-meadow box"> -->
+    </div>
+    <!-- end <div class="col-md-6"> -->
+    <!--
+    <div class="col-md-6">
+    </div>
+    -->
+    <!-- end <div class="col-md-6"> -->
+</div>
+
+
+<?php $this->load->view('app.min.inc.php'); ?>
+<?php $this->load->view('laporan/js/lap_kk.js.php'); ?>
+
